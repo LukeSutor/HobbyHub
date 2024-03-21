4f8cfe3591c13bab4c9a95e5ef9e1710f302ad5f
Luke Sutor <64111222+LukeSutor@users.noreply.github.com>
2024-03-21 18:39:49 -0400

    Revert "Merge 1"

--- src/index.js
+++ src/index.js
@@ -1,40 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Import context
-import { AppProvider } from "./AppContext";
-
-// Import pages
-import Landing from "./pages/landing";
-import Login from "./pages/login";
-import Signup from "./pages/signup";
-import Profile from "./pages/profile";
-import Matches from "./pages/matches";
-import Login_Unsuccess from "./pages/login_unsuccessful";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <AppProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/login_unsuccessful" element={<Login_Unsuccess />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/matches" element={<Matches />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </AppProvider>
-  </>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
