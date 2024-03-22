a22d91458c25f1c4500844a84eb40da8be7e9e34
Luke Sutor <lukesutor@gmail.com>
2024-03-21 20:19:45 -0400

    fix delete match bug

--- src/pages/matches.js
+++ src/pages/matches.js
@@ -36,7 +36,7 @@ export default function Matches() {
             >
               <p>{match.user.email}</p>
               <button
-                onClick={() => deleteMatch(match.id)}
+                onClick={() => console.log("DELETING")}
                 className="bg-red-200 border border-red-400 font-semibold px-3 py-2 rounded-lg shadow"
               >
                 Unmatch
